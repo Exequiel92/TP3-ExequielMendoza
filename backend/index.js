308eba76e3de2f2dcf4c9a4f4b4ed81eb7b65dd4
Exequiel José Mendoza <exejm92@gmail.com>
2025-11-07 14:55:13 -0300

    se agregó index.js y db.js

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { conectarDB } from "./db.js";
+import alumnosRouter from "./alumnos.js";
+import materiasRouter from "./materias.js";
+import notasRouter from "./notas.js";
+//import usuariosRouter from "./usuarios.js";
+//import authRouter, { authConfig } from "./auth.js";
+
+conectarDB();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+//authConfig();
+
+app.use("/alumnos", alumnosRouter);
+app.use("/materias", materiasRouter);
+app.use("/notas", notasRouter);
+//app.use("/usuarios", usuariosRouter);
+
+app.listen(port, () => {
+  console.log(`La aplicación esta funcionando en el puerto ${port}`);
+});
